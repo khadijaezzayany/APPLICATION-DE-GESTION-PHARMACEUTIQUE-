e7a21aa2ef547e02a3ba9968fca1f0321c1da611
khadijaezzayany <khadijaezzayany8@gmail.com>
2020-10-26 13:33:19 +0100

    style page home

---
+++ index.php
@@ -0,0 +1,31 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="css/style.css">
+    <title>Document</title>
+</head>
+
+<body>
+    <div class="header">
+        <div class="header__nav">
+            <ul>
+                <li><a href="">Home</a></li>
+                <li><a href="">News</a></li>
+                <li><a href="">About Us</a></li>
+                <li><a href="">Login</a></li>
+            </ul>
+        </div>
+        <h1>PHARMACY <br> <span> Projet</span></h1>
+        <p>Le Lorem Ipsum est simplement du faux texte employé dans
+            la composition et la mise en page avant impression. Le Lorem
+            Ipsum est le faux texte standard de l'imprimerie depuis
+            les années 1500,</p>
+        <a id="but" href="">LOGIN</a>
+    </div>
+
+</body>
+
+</html>
